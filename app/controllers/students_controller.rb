85f4d440a3f6d29176b15a0bdc0dacdd6878289c
jpv2018 <jason.viola@mavs.uta.edu>
2020-11-15 18:07:47 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,11 +6,26 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find_by(params[:id])
+  end
+
+  def activate
+    @student = Student.find_by(params[:id])
+    @student.activate_student
+    redirect_to student_path(@student)
   end
 
   private
 
     def set_student
       @student = Student.find(params[:id])
+    end
+
+    def activate_student
+      if self.active?
+        self.update_attribute(:active, false)
+      else
+        self.update_attribute(:active, true)
+      end
     end
 end
